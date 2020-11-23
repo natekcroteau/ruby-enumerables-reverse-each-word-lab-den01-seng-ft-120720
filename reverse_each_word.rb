3c979a4823a2ed0007e831a436225aadd118487b
Nate Croteau <natekcroteau@gmail.com>
2020-11-23 23:04:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
-def reverse_each_word(string)
-  arr = string.split(" ")
-  arr.collect{|str|str.reverse}.join(" ")
-  
+def reverse_each_word(string)            
+  arr = string.split(" ")                   #new arr = the string split up and stored as an array
+  arr.collect{|str|str.reverse}.join(" ")    #the new arr is itterated over reversing each object then join turns arr into one string 
 end
